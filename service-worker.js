3affaff7ee4eda6249d4aa101c2302005d9bf903
Emilie f <omika.dg@gmail.com>
2021-09-18 12:09:20 -0400

    commit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,23 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v5';
+const CACHE_NAME = 'static-cache-v9';
+
+self.addEventListener('activate', (evt) => {
+console.log('[ServiceWorker] Activate');
+
+//Remove previous cached data from disk.
+evt.waitUntil(
+    caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                return caches.delete(key);
+            }
+        }));
+    })
+);
+
+self.clients.claim();
+});
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -19,49 +37,28 @@ self.addEventListener('install', (evt) => {
     // Precache static resources here.
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
         })
-        );
-        self.skipWaiting();
-        }); 
-  
-        self.addEventListener('activate', (evt) => {
-            console.log('[ServiceWorker] Activate');
-            //Remove previous cached data from disk.
-            evt.waitUntil(
-            caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache',
-            key);
-            return caches.delete(key);
-            }
-            }));
-            })
-            );
-            self.clients.claim();
-            });
-            
-    self.addEventListener('fetch', (evt) => {
+    );
+    self.skipWaiting();
+});
+
+
+self.addEventListener('fetch', (evt) => {
     console.log('[ServiceWorker] Fetch', evt.request.url);
-
-
-    self.addEventListener('fetch', (evt) => {
-        console.log('[ServiceWorker] Fetch', evt.request.url);
-        //Add fetch event handler here.
-        if (evt.request.mode !== 'navigate') {
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
         // Not a page navigation, bail.
         return;
-        }
-        evt.respondWith(
+    }
+    evt.respondWith(
         fetch(evt.request)
-        .catch(() => {
-        return caches.open(CACHE_NAME)
-        .then((cache) => {
-        return cache.match('https://omika17.github.io/integration/index.html' );
-        });
-        })
-        );
-        });
-        });
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('https://omika17.github.io/Interface/offline.html');
+                    });
+            })
+    );
+}); 
